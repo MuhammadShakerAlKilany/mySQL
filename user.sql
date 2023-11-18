905fd0c9eccc2fa592e34a865a67e02f4b90c01b
root <root@DESKTOP-EEFDFDQ>
2023-11-18 11:37:25 +0200

    show grants

--- user.sql
+++ user.sql
@@ -3,4 +3,5 @@ select  User from mysql.user;
 select user,host from mysql.user;
 grant all privileges on db_test.* to 'muhammad'@'localhost' ;
 revoke all privileges on db_test.* from 'muhammad'@'localhost';
+show grants for 'muhammad'@'localhost';
 drop user 'muhammad'@'localhost';
