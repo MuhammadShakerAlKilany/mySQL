f2915efc8332a8aaf68675312099c4a247461692
root <root@DESKTOP-EEFDFDQ>
2023-11-18 12:20:07 +0200

    order by

--- table.sql
+++ table.sql
@@ -1,1 +1,5 @@
 create table table_test( id int not null AUTO_INCREMENT, name varchar(255) not null , primary key (id));
+select * from table_test;
+ insert into table_test (name) values ('muhammad');
+select * from table_test limit 5;
+ select * from db_test.table_test order by name ;
